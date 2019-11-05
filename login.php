8a1f639610522e6511f7b7de959513d392a9305a
ethanrei <ethanreiarevalo@gmail.com>
2019-11-05 16:44:34 +0800

    shorten

--- login.php
+++ login.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.css">
-    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="icon" href="image/OKAERI_tab.png" type="image/gif" sizes="16x16"> 
+    <?php include('css.php');?>
     <link rel="stylesheet" href="css/logincss.css">
     <title>Log In to Okaeri!</title>
 </head>
@@ -29,9 +26,6 @@
         </div>
     </section>
 <!--SCRIPT-->
-    <script src="js/main.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+<?php include('script.php');?>
 </body>
 </html>
