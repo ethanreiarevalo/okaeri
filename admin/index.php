b6886277bb0e359cb8b6b46d536fdd8f739d25e0
munggohan <38007456+munggohan@users.noreply.github.com>
2019-11-07 15:15:37 +0800

    file upload

--- admin/index.php
+++ admin/index.php
@@ -15,8 +15,67 @@ if(isset($_SESSION ["userID"])){
     $price = $_POST['price'];
     $image = $_POST['image'];
 
-    if(mysqli_query($connection,"INSERT INTO products VALUES ()")){
+    if(mysqli_query($connection,"INSERT INTO products VALUES (null, '$title', '$author', '$publisher', '$type',
+    '$language', '$dReceived', '$dPublished', null, 'product_image/".$image."', '$stock', '$price')")){
+      echo "uploaded to database";
+
+      $target_dir = "C:/xampp/htdocs/www.okaeri.com/product_images/";
+      $target_file = $target_dir . basename($_FILES["image"]["name"]);
+      $uploadOk = 1;
+      $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
       
+      // Check if image file is a actual image or fake image
+      if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if($check !== false) {
+          echo "File is an image - " . $check["mime"] . ".";
+          $uploadOk = 1;
+        } else {
+          echo "File is not an image.";
+          $uploadOk = 0;
+        }
+      }else{
+        echo 'error in uploading';
+      }
+      
+      // Check if file already exists
+      if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+      }
+      
+      // Check file size
+      if ($_FILES["fileToUpload"]["size"] > 500000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+      }
+      
+      // Allow certain file formats
+      if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+      && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+      }
+      
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+      } else {
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+          echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+          $message = 'File Successfully Added';
+          echo "<script type='text/javascript'>alert('$message');</script>";
+                
+          header("location:admin");
+        } else {
+          echo "Sorry, there was an error uploading your file.";
+        }
+      }
+
+
+
+
     }
   }
     
@@ -79,9 +138,9 @@ if(isset($_SESSION ["userID"])){
                     </div>
                     <input type="date" class="form-control" name="dReceived">
                 </div>
-                <input type="text" class="form-control mb-1" placeholder="Stock" pattern= "[0-9]" name="stock">
-                <input type="text" class="form-control mb-1" placeholder="Price" pattern= "[0-9]" name="price">
-                <input type="file" class="form-control-file" placeholder="Upload Photo" accept="product_image/*" name="image">
+                <input type="text" class="form-control mb-1" placeholder="Stock" name="stock">
+                <input type="text" class="form-control mb-1" placeholder="Price" name="price">
+                <input type="file" class="form-control-file" placeholder="Upload Photo" accept="product_image/*" id="image" name="image">
                 <button class="btn btn-warning" type="submit">Submit</button>
               </form>
             </div>
