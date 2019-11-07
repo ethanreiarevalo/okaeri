b03d50d6ac08a4ab6df374130fe3954c075e2fee
ethanrei <ethanreiarevalo@gmail.com>
2019-11-07 13:11:30 +0800

    admin side

---
+++ admin/css.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.css">
+<link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="icon" href="../image/OKAERI_tab.png" type="image/gif" sizes="16x16"> 
---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include('css.php');?>
+    <title>OKAERI ADMIN</title>
+</head>
+<body>
+<?php include('nav.php');?>
+
+<!-- SCRIPT     -->
+<?php include('script.php');?>
+</body>
+</html>
