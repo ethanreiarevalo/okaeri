ba571912779c8e9aed1851fa18e98f37b6afc9f8
munggohan <38007456+munggohan@users.noreply.github.com>
2019-11-07 14:54:13 +0800

    database

--- admin/index.php
+++ admin/index.php
@@ -2,15 +2,25 @@
 session_start(); 
 include('../connection.php');
 if(isset($_SESSION ["userID"])){
-    // $user_id = $_SESSION["userID"];
-    // $get_record = mysqli_query($connection, "SELECT * FROM accountdetailstbl WHERE userID= '$user_id'");
-    // $row = mysqli_fetch_array($get_record);
-    // if($row['userID'] = $user_id){
-    //     $db_name = $row ["userFname"];
-    //     // $db_email = $row["email"];
-    // }     
+  if($_SERVER ["REQUEST_METHOD"] == "POST"){
+    
+    $title = $_POST['title'];
+    $author = $_POST['author'];
+    $publisher = $_POST['publisher'];
+    $language = $_POST['language'];
+    $type = $_POST['type'];
+    $dPublished = $_POST['dPublished'];
+    $dReceived = $_POST['dReceived'];
+    $stock = $_POST['stock'];
+    $price = $_POST['price'];
+    $image = $_POST['image'];
+
+    if(mysqli_query($connection,"INSERT INTO products VALUES ()")){
+      
+    }
+  }
+    
 }else if(!isset($_SESSION ["userID"])){
-    // echo "<script language = 'javascript'>alert('You must login first!')</script>";
     echo "<script>window.location.href='../index.php';</script>";   
 }
 
@@ -37,20 +47,21 @@ if(isset($_SESSION ["userID"])){
     <section class="row">
         <div id="fields" class="bg-dark col-xl-4">
             <div class="jumbotron d-block text-center text-white bg-transparent">
+              <form action="<?php htmlspecialchars("PHP_SELF"); ?>" method="post"> 
                 <p class="lead">Add Item</p>
                 <hr class="my-2 bg-white">
-                <input type="text" class="form-control mb-1" placeholder="Title">
-                <input type="text" class="form-control mb-1" placeholder="Author">
-                <input type="text" class="form-control mb-1" placeholder="Publisher">
+                <input type="text" class="form-control mb-1" placeholder="Title" name="title">
+                <input type="text" class="form-control mb-1" placeholder="Author" name="author">
+                <input type="text" class="form-control mb-1" placeholder="Publisher" name="publisher">
                 <div class="input-group mb-1">
-                    <select class="custom-select" id="inputGroupSelect01">
+                    <select class="custom-select" id="inputGroupSelect01" name="language">
                       <option selected>Language</option>
                       <option value="1">Japanese</option>
                       <option value="2">English</option>
                     </select>
                 </div>
                 <div class="input-group mb-1">
-                    <select class="custom-select" id="inputGroupSelect02">
+                    <select class="custom-select" id="inputGroupSelect02" name="type">
                       <option selected>Type of Product</option>
                       <option value="1">Manga</option>
                       <option value="2">Light Novel</option>
@@ -60,18 +71,19 @@ if(isset($_SESSION ["userID"])){
                     <div class="input-group-prepend">
                       <label class="input-group-text" for="inputGroupSelect01">Date Published</label>
                     </div>
-                    <input type="date" class="form-control">
+                    <input type="date" class="form-control" name="dPublished">
                 </div>
                 <div class="input-group mb-1">
                     <div class="input-group-prepend">
-                      <label class="input-group-text" for="inputGroupSelect01">Date Recieved</label>
+                      <label class="input-group-text" for="inputGroupSelect01">Date Received</label>
                     </div>
-                    <input type="date" class="form-control">
+                    <input type="date" class="form-control" name="dReceived">
                 </div>
-                <input type="text" class="form-control mb-1" placeholder="Stock" pattern= "[0-9]" >
-                <input type="text" class="form-control mb-1" placeholder="Price" pattern= "[0-9]" >
-                <input type="file" class="form-control-file" placeholder="Upload Photo" accept="image">
-                <button class="btn btn-warning">Submit</button>
+                <input type="text" class="form-control mb-1" placeholder="Stock" pattern= "[0-9]" name="stock">
+                <input type="text" class="form-control mb-1" placeholder="Price" pattern= "[0-9]" name="price">
+                <input type="file" class="form-control-file" placeholder="Upload Photo" accept="product_image/*" name="image">
+                <button class="btn btn-warning" type="submit">Submit</button>
+              </form>
             </div>
         </div>
         <div id="tables" class="col-xl-8">
---
+++ okaeri.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2019 at 07:51 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `okaeri`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `productID` int(11) NOT NULL,
+  `productTitle` varchar(20) NOT NULL,
+  `productAuthor` varchar(20) NOT NULL,
+  `productPublisher` varchar(30) NOT NULL,
+  `productType` varchar(10) NOT NULL,
+  `productLanguage` varchar(20) NOT NULL,
+  `productDateReceived` date NOT NULL,
+  `productDatePublished` date NOT NULL,
+  `productGenre` varchar(100) NOT NULL,
+  `productImage` varchar(50) NOT NULL,
+  `productStock` int(3) NOT NULL,
+  `productPrice` double(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`productID`, `productTitle`, `productAuthor`, `productPublisher`, `productType`, `productLanguage`, `productDateReceived`, `productDatePublished`, `productGenre`, `productImage`, `productStock`, `productPrice`) VALUES
+(1, 'Komi Can\'t Communica', 'Tomohito Oda', 'Viz Media', 'Manga', 'English', '0000-00-00', '0000-00-00', 'School Life, Romance, Comdey', 'product_image/Komi-san.jpg', 10, 100.00),
+(2, 'Saint Young Men', 'Hikaru Nakamura', 'Kodansha', 'Manga', 'English', '0000-00-00', '0000-00-00', 'Comedy', 'product_image/saintyoungmen.jpg', 20, 100.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `useraccounts`
+--
+
+CREATE TABLE `useraccounts` (
+  `userID` int(11) NOT NULL,
+  `userEmail` varchar(50) NOT NULL,
+  `userPassword` varchar(50) NOT NULL,
+  `userType` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `useraccounts`
+--
+
+INSERT INTO `useraccounts` (`userID`, `userEmail`, `userPassword`, `userType`) VALUES
+(1, 'Admin', 'Admin', 'Admin'),
+(2, 'carloangeles342@gmail.com', 'asdfjklh', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdetails`
+--
+
+CREATE TABLE `userdetails` (
+  `email` varchar(50) NOT NULL,
+  `fName` varchar(50) NOT NULL,
+  `lName` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contactNo` int(11) NOT NULL,
+  `birthdate` date NOT NULL,
+  `sex` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `userdetails`
+--
+
+INSERT INTO `userdetails` (`email`, `fName`, `lName`, `address`, `contactNo`, `birthdate`, `sex`) VALUES
+('carloangeles342@gmail.com', 'Carlo', 'Angeles', 'Alfonso, Cavite', 2147483647, '1996-11-01', 'Male');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productID`);
+
+--
+-- Indexes for table `useraccounts`
+--
+ALTER TABLE `useraccounts`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `useraccounts`
+--
+ALTER TABLE `useraccounts`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
