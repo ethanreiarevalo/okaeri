509a0224456a9fe9c5936aff0abe355339080c86
ethanrei <ethanreiarevalo@gmail.com>
2019-11-12 13:10:54 +0800

    popup index update

--- item.php
+++ item.php
@@ -41,12 +41,50 @@
                             <input type="text" class="form-control mx-1" id="integer_value">
                             <button id="plus-button" class="btn btn-primary mx-1" onclick="plusValue()">+</button>
                         </div>
-                        <button class="btn btn-warning col-xl-2">Add to Cart</button>
+                        <button id="button" class="btn btn-warning col-xl-2" onclick="popup()">Add to Cart</button>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
+    <!-- DO NOT CHANGE THIS SECTION -->
+    <!-- POPUP LOGIN FIRST -->
+    <!-- DO NOT CHANGE -->
+    <head>
+        <style>
+            .pop{
+                position:fixed;
+                width: 100%;
+                height: 100vh;
+                top: 23%;
+            }
+            .popup{
+                display:none;
+            }
+            #modal img{
+                width:100px;
+                height:150px;
+            }
+            .cl{
+                height: 20px;
+                width: 20px;
+                border-radius: 50%;
+                cursor: pointer;
+            }
+        </style>
+    </head>
+    <div id="modal" class="popup">
+        <div class="row w-100 justify-content-center">
+            <div class="jumbotron bg-dark text-center">
+                <div class="cl row justify-content-center bg-warning text-dark text-center" onclick="popup()">x</div>
+                <img src="image/popup-image.png" alt="">
+                <p class="lead text-white">You must login first!</p>
+            </div>
+        </div>
+    </div>
+
+
     <!-- SCRIPT FOR THE INPUT BUTTON -->
     <!-- DO NOT CHANGE -->
     <script>
@@ -67,6 +105,17 @@
 
             if(checknegative < 0){
                 document.getElementById("integer_value").value = 0;
+            }
+        }
+
+        var t = document.getElementById("modal");
+
+        function popup(){
+            if (t.className === "popup"){
+                t.className = "pop";
+            }
+            else{
+                t.className = "popup";
             }
         }
     </script>
