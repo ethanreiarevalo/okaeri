05b9108bed30df15176ab8644562061fc4700345
ethanrei <ethanreiarevalo@gmail.com>
2019-11-12 22:25:27 +0800

    Update cart.php

--- user/cart.php
+++ user/cart.php
@@ -11,6 +11,37 @@
     <header>
         <?php include('nav.php');?>
     </header>
+    <section id="tablecart" class="table-responsive">
+        <table class="table table-bordered table-striped text-center mt-5">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Cover</th>
+                    <th>Title</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tr>
+                <td>
+                    <img src="" alt="">
+                </td>
+                <td>
+                    Komi-san
+                </td>
+                <td>
+                    2
+                    <button>edit</button>
+                </td>
+                <td>
+                    200
+                </td>
+            </tr>
+
+        </table>
+    </section>
+    <section id="checkout">
+
+    </section>
     <?php include('script.php');?>
 </body>
 </html>
