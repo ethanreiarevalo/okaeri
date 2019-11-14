aecc3dacff2d939c9a8a608d67df9dc946c2d4b6
ethanrei <ethanreiarevalo@gmail.com>
2019-11-14 09:57:30 +0800

    updates

--- admin/index.php
+++ admin/index.php
@@ -142,21 +142,19 @@ if(isset($_SESSION ["userID"])){
                 <input type="text" class="form-control mb-1" placeholder="Stock" name="stock">
                 <input type="text" class="form-control mb-1" placeholder="Price" name="price">
                 <div class="row">
-                  <div class="container custom-control custom-checkbox col-xl-1">
-                    <input type="checkbox" class="custom-control-input" id="adventure">
-                    <label class="custom-control-label" for="adventure">Adventure</label>
-                    <input type="checkbox" class="custom-control-input" id="romance">
-                    <label class="custom-control-label" for="romance">Romance</label>
+                  <div class="container">
+                    <div class="container custom-control custom-checkbox">
+                      <input type="checkbox" class="custom-control-input" id="adventure">
+                      <label class="custom-control-label" for="adventure">Adventure</label>
+                    </div>
+                    <div class="container custom-control custom-checkbox">
+                      <input type="checkbox" class="custom-control-input" id="romance">
+                      <label class="custom-control-label" for="romance">Romance</label>
+                    </div>
+                    <div class="container custom-control custom-checkbox">
                     <input type="checkbox" class="custom-control-input" id="sliceoflife">
-                    <label class="custom-control-label" for="sliceoflife">Slice of Life</label>
-                  </div>
-                  <div class="container custom-control custom-checkbox col-xl-1">
-                    <input type="checkbox" class="custom-control-input" id="action">
-                    <label class="custom-control-label" for="action">Action</label>
-                    <input type="checkbox" class="custom-control-input" id="horror">
-                    <label class="custom-control-label" for="horror">Horror</label>
-                    <input type="checkbox" class="custom-control-input" id="fantasy">
-                    <label class="custom-control-label" for="fantasy">Fantasy</label>
+                      <label class="custom-control-label" for="sliceoflife">Slice of Life</label>
+                    </div>
                   </div>
                 </div>
                 <textarea name="summary" id="summary" class="form-control" cols="30" rows="10" placeholder="Enter Product Summary here"></textarea>
