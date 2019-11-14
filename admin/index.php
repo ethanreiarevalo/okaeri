7a75d1f5ed46b135d592270b612ceec4d32cc47d
ethanrei <ethanreiarevalo@gmail.com>
2019-11-14 10:08:09 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -151,7 +151,7 @@ if(isset($_SESSION ["userID"])){
                 <input type="text" class="form-control mb-1" placeholder="Stock" name="stock">
                 <input type="text" class="form-control mb-1" placeholder="Price" name="price">
                 <div class="row">
-                  <div class="container col-xl-5">
+                  <div class="container mb-1 col-xl-5">
                     <div class="container custom-control custom-checkbox">
                       <input type="checkbox" class="custom-control-input" id="adventure">
                       <label class="custom-control-label float-left" for="adventure">Adventure</label>
@@ -166,7 +166,7 @@ if(isset($_SESSION ["userID"])){
                     </div>
                   </div>
 
-                  <div class="container col-xl-5">
+                  <div class="container mb-1 col-xl-5">
                     <div class="container custom-control custom-checkbox">
                       <input type="checkbox" class="custom-control-input" id="action">
                       <label class="custom-control-label float-left" for="action">Action</label>
@@ -181,8 +181,9 @@ if(isset($_SESSION ["userID"])){
                     </div>
                   </div>
                 </div>
-                <textarea name="summary" id="summary" class="form-control" cols="30" rows="10" placeholder="Enter Product Summary here"></textarea>
-                <input type="file" id="fileToUpload" name="fileToUpload" class="form-control-file" placeholder="Upload Photo" accept="image/*">
+                <textarea name="summary" id="summary" class="form-control mb-1" cols="30" rows="10" placeholder="Enter Product Summary here"></textarea>
+                <label for="fileToUpload" class="float-left">Upload a cover image (200x300)</label>
+                <input type="file" id="fileToUpload" name="fileToUpload" class="form-control-file mb-1" placeholder="Upload Photo" accept="image/*">
                 <button class="btn btn-warning" type="submit">Submit</button>
               </form>
             </div>
