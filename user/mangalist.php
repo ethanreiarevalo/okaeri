df12ce96ae78e4be58e56365fc7e7e6983d93aab
ethanrei <ethanreiarevalo@gmail.com>
2019-11-14 17:36:37 +0800

    update

--- user/mangalist.php
+++ user/mangalist.php
@@ -18,33 +18,35 @@
         <?php include('nav.php');?>
     </header>
     <section class="row">
-    <div id="options" class="col-xl-3">
+        <div id="options" class="col-xl-3">
             <div class="jumbotron bg-transparent">
-            <form action="" method="">
-                <h5>Language</h5>
-                <div class="custom-control custom-radio">
-                  <input type="radio" class="custom-control-input" id="Japanese" name="example1" value="Japanese">
-                  <label class="custom-control-label" for="Japanese">Japanese</label>
-                </div>
-                <div class="custom-control custom-radio">
-                  <input type="radio" class="custom-control-input" id="English" name="example1" value="English">
-                  <label class="custom-control-label" for="English">English</label>
-                </div>
-                <hr class="my-3">
-                <h5>Genre</h5>
-                <div class="container custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" name="genre[]" id="action">
-                  <label class="custom-control-label float-left" for="action">Action</label>
-                </div>
-                <div class="container custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" name="genre[]" id="horror">
-                  <label class="custom-control-label float-left" for="horror">Horror</label>
-                </div>
-                <div class="container custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" name="genre[]" id="fantasy">
-                  <label class="custom-control-label float-left" for="fantasy">Fantasy</label>
-                </div>
-            </form>
+                <h3>Manga List</h3>
+                <p class="mb-3">Sort By:</p>
+                <form action="" method="">
+                    <h5>Language</h5>
+                    <div class="custom-control custom-radio">
+                      <input type="radio" class="custom-control-input" id="Japanese" name="example1" value="Japanese">
+                      <label class="custom-control-label" for="Japanese">Japanese</label>
+                    </div>
+                    <div class="custom-control custom-radio">
+                      <input type="radio" class="custom-control-input" id="English" name="example1" value="English">
+                      <label class="custom-control-label" for="English">English</label>
+                    </div>
+                    <hr class="my-3 bg-warning">
+                    <h5>Genre</h5>
+                    <div class="container custom-control custom-checkbox">
+                      <input type="checkbox" class="custom-control-input" name="genre[]" id="action">
+                      <label class="custom-control-label float-left" for="action">Action</label>
+                    </div>
+                    <div class="container custom-control custom-checkbox">
+                      <input type="checkbox" class="custom-control-input" name="genre[]" id="horror">
+                      <label class="custom-control-label float-left" for="horror">Horror</label>
+                    </div>
+                    <div class="container custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" name="genre[]" id="fantasy">
+                      <label class="custom-control-label float-left" for="fantasy">Fantasy</label>
+                    </div>
+                </form>
             </div>
         </div>
         <div id="card" class="mt-5">
@@ -57,7 +59,7 @@
                     while($row = mysqli_fetch_array($result)){
                 ?>
                     <div class="card m-3 border border-warning" style="width:220px; height:500px;">
-                        <img class="card-img-top" src="<?php echo $row['productImage']; ?>" alt="">
+                        <img class="card-img-top" src="../<?php echo $row['productImage']; ?>" alt="">
                         <div class="card-body text-center">
                             <p class="card-title"><?php echo $row['productTitle']; ?></p>
                             <p class="card-text">Price: <?php echo $row['productPrice'];?></p>
