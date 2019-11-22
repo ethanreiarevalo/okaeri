5098076b08ee23bfe05d379697419e4401740e9a
ethanrei <ethanreiarevalo@gmail.com>
2019-11-22 11:03:47 +0800

    update

--- user/account-settings.php
+++ user/account-settings.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+    <?php include('css.php');?>
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <?php include('nav.php');?>
+    </header>
+    <?php include('script.php');?>
 </body>
 </html>
