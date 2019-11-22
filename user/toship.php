24a774ed7051dfd891094acc32a8d85c504b77a7
ethanrei <ethanreiarevalo@gmail.com>
2019-11-22 11:40:27 +0800

    update track

--- user/toship.php
+++ user/toship.php
@@ -11,40 +11,53 @@
     <header>
         <?php include('nav.php');?>
     </header>
-    <section id="sidenav" class="col-xl-2">
-        <a href="trackall.php">
-            <div class="text-center mt-4 py-3">
-                All
+    <section class="container">
+        <div class="row">
+            <div id="sidenav" class="col-xl-2">
+                <a href="trackall.php">
+                    <div class="text-center mt-4 py-3">
+                        All
+                    </div>
+                </a>
+                <a href="topay.php">
+                    <div class="text-center py-3">
+                        To Pay
+                    </div>
+                </a>
+                <a href="toship.php">
+                    <div class="text-center border-right border-warning py-3">
+                        To Ship
+                    </div>
+                </a>
+                <a href="torecieve.php">
+                    <div class="text-center py-3">
+                        To Recieve
+                    </div>
+                </a>
+                <a href="completed.php">
+                    <div class="text-center py-3">
+                        Completed
+                    </div>
+                </a>
+                <a href="cancelled.php">
+                    <div class="text-center py-3">
+                        Cancelled
+                    </div>
+                </a>
             </div>
-        </a>
-        <a href="topay.php">
-            <div class="text-center py-3">
-                To Pay
+            <div id="table">
+                <div class="table table-responsive table-bordered m-5">
+                    <table>
+                        <tr class="thead-dark">
+                            <th>Product Cover</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                        </tr>
+                    </table>
+                </div>
             </div>
-        </a>
-        <a href="toship.php">
-            <div class="text-center py-3 border-right border-warning">
-                To Ship
-            </div>
-        </a>
-        <a href="torecieve.php">
-            <div class="text-center py-3">
-                To Recieve
-            </div>
-        </a>
-        <a href="completed.php">
-            <div class="text-center py-3">
-                Completed
-            </div>
-        </a>
-        <a href="cancelled.php">
-            <div class="text-center py-3">
-                Cancelled
-            </div>
-        </a>
-    </section>
-    <section>
-
+        </div>
     </section>
     <?php include('script.php');?>
 </body>
