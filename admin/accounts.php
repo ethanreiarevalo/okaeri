92dd2f1d0a2db2e7c6da8bec702b5edd57580190
ethanrei <ethanreiarevalo@gmail.com>
2019-11-23 11:27:29 +0800

    update

--- admin/accounts.php
+++ admin/accounts.php
@@ -16,8 +16,8 @@
         
         <hr class="my-4">
         <div class="table table-responsive">
-            <table class="table table-responsive table-bordered w-100">
-                <tr class="thead-dark">
+            <table id="table" class="table table-responsive table-bordered w-100">
+                <tr class="thead-dark text-center">
                     <th>
                         Customer Email
                     </th>
@@ -44,7 +44,7 @@
                     if(mysqli_num_rows($result) > 0){
                         while($row = mysqli_fetch_array($result)){
                     ?>
-                        <tr>
+                        <tr class="text-center">
                             <td>
                                 <?php echo $row['userEmail']; ?>
                             </td>
@@ -59,8 +59,15 @@
                                 <?php echo $row['contactNo']; ?>
                             </td>
                             <td>
-                                <button class="btn btn-primary"><i><?php echo $row['status']?></i></button>
+                               <?php 
+                                 if($row['status'] === "Inactive"){
+                                    echo "<button id='inactive' class='btn btn-danger'><i>".$row['status']."</i></button>";
+                                } else{
+                                    echo "<button id='active' class='btn btn-primary'><i>".$row['status']."</i></button>";
+                                }
+                                ?>
                             </td>
+                            
                         </tr> 
                     <?php 
                         }}
@@ -98,6 +105,34 @@
             </form>
         </div>
     </section>
+
+    <!-- <script>
+        function press(){
+            var table = document.getElementById('table');
+                var active = '<button id="status" class="btn btn-primary"><i>Active</i></button>';
+                var inactive = '<button id="status" onclick="press()" class="btn btn-primary"><i>Inactive</i></button>';
+                for(var i = 1; i < table.rows.length; i++)
+                {
+                    table.rows[i].onclick = function()
+                    {
+                         rIndex = this.rowIndex;
+                        // alert(this.cells[4].innerHTML);
+                        var thevalue  = this.cells[4].innerHTML.toString();
+                        if(thevalue ===  toString(inactive)){
+                            alert("try");
+                        }else{
+                            alert(thevalue+inactive);
+                        }
+                        
+                        // document.getElementById('itemcode').value = this.cells[0].innerHTML;
+                        // document.getElementById("p_name").value = this.cells[1].innerHTML;
+                        // document.getElementById("author").value = this.cells[2].innerHTML;
+                        // document.getElementById("publisher").value = this.cells[3].innerHTML;
+                        // document.getElementById("language").value = this.cells[4].innerHTML;
+                    };
+                }
+        }
+    </script> -->
     <?php include('script.php');?>
 </body>
 </html>
--- admin/sales.php
+++ admin/sales.php
@@ -6,12 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php include('css.php');?>
     <title>Manage Sales</title>
+    <style>
+    </style>
 </head>
 <body>
     <header>
         <?php include('nav.php');?>
     </header>
-    <section>
+    <section class="container">
+        <div class="mt-5">
+            <h5>Total Sales</h5>
+            <p class="lead">This Month:</p>
+            <p class="lead">This Week:</p>
+            <p class="lead">Today:</p>
+        </div>
+        <div class="row justify-content-between">
+            <p>Total Sales: </p>
+            <div class="input-group w-25 mb-1">
+                <select class="custom-select w-25" id="inputGroupSelect01" name="month">
+                  <option value="January">January</option>
+                  <option value="February">February</option>
+                  <option value="March">March</option>
+                  <option value="April">April</option>
+                  <option value="May">May</option>
+                  <option value="June">June</option>
+                  <option value="July">July</option>
+                  <option value="August">August</option>
+                  <option value="September">September</option>
+                  <option value="October">October</option>
+                  <option value="November">November</option>
+                  <option value="February">December</option>
+                </select>
+                <button class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+        <center>
         <div id="table" class="table-responsive mt-5">
             <table class="table table-responsive table-bordered">
                 <tr class="thead-dark">
@@ -35,6 +64,7 @@
                 </tr>
             </table>
         </div>
+        </center>
     </section>
     <?php include('script.php');?>
 </body>
