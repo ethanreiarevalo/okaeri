dfdfd363d6ff6d9605d5b33bcd8b1c8c629c057f
ethanrei <ethanreiarevalo@gmail.com>
2019-11-23 11:48:46 +0800

    delivery update

--- admin/delivery.php
+++ admin/delivery.php
@@ -11,6 +11,35 @@
     <header>
         <?php include('nav.php');?>
     </header>
+    <section class="container">
+        <div class="row">
+            <div class="jumbotron bg-transparent w-100">
+                <h1 class="display-4">Delivery Update</h1>
+                <hr class="my-4">
+                <div class="table table-responsive">
+                    <table id="table" class="table table-bordered">
+                        <tr class="thead-dark text-center">
+                            <th>
+                                Product
+                            </th>
+                            <th>
+                                Quantity
+                            </th>
+                            <th>
+                                Total Price
+                            </th>
+                            <th>
+                                Tracking Number
+                            </th>
+                            <th>
+                                Delivery Date
+                            </th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
     <?php include('script.php');?>
 </body>
 </html>
