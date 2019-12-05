c29f12477f2fd11da62fcc5b7c22c54607f6ec74
ethanrei <ethanreiarevalo@gmail.com>
2019-12-05 16:07:10 +0800

    SQL and 5 manga and ln
    Added

--- okaeri.sql
+++ okaeri.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2019 at 04:51 AM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Generation Time: Dec 05, 2019 at 09:05 AM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,96 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `okaeri`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `10cart`
---
-
-CREATE TABLE `10cart` (
-  `productID` int(10) DEFAULT NULL,
-  `amount` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
---
--- Dumping data for table `10cart`
---
-
-INSERT INTO `10cart` (`productID`, `amount`) VALUES
-(4, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `10purchases`
---
-
-CREATE TABLE `10purchases` (
-  `productID` int(10) DEFAULT NULL,
-  `amount` int(10) DEFAULT NULL,
-  `datePurchase` date DEFAULT NULL,
-  `salesID` int(10) DEFAULT NULL,
-  `paymentMethod` varchar(16) NOT NULL,
-  `orderStatus` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
---
--- Dumping data for table `10purchases`
---
-
-INSERT INTO `10purchases` (`productID`, `amount`, `datePurchase`, `salesID`, `paymentMethod`, `orderStatus`) VALUES
-(0, 1, '2019-11-22', 9, 'Cash On Delivery', 'For Delivery'),
-(3, 3, '2019-11-22', 9, 'Cash On Delivery', 'For Delivery'),
-(0, 1, '2019-11-22', 10, 'Cash On Delivery', 'For Delivery'),
-(4, 1, '2019-11-22', 10, 'Cash On Delivery', 'For Delivery'),
-(0, 1, '2019-11-22', 11, 'Cash On Delivery', 'For Delivery'),
-(5, 3, '2019-11-22', 11, 'Cash On Delivery', 'For Delivery'),
-(0, 1, '2019-11-23', 12, 'Cash On Delivery', 'Undelivered'),
-(3, 1, '2019-11-23', 12, 'Cash On Delivery', 'Undelivered'),
-(0, 1, '2019-11-23', 13, 'Cash On Delivery', 'Delivered'),
-(4, 2, '2019-11-23', 13, 'Cash On Delivery', 'Delivered'),
-(0, 1, '2019-11-23', 14, 'Cash On Delivery', 'Undelivered'),
-(1, 2, '2019-11-23', 14, 'Cash On Delivery', 'Cancelled'),
-(2, 2, '2019-11-23', 14, 'Cash On Delivery', 'Cancelled'),
-(0, 1, '2019-11-24', 15, '', 'Undelivered'),
-(1, 2, '2019-11-24', 15, '', 'Cancelled'),
-(0, 1, '2019-11-24', 16, 'Cash On Delivery', 'Undelivered'),
-(3, 5, '2019-11-24', 16, 'Cash On Delivery', 'Cancelled'),
-(0, 1, '2019-11-24', 17, 'Cash On Delivery', 'Undelivered'),
-(0, 1, '2019-11-24', 18, 'Cash On Delivery', 'Undelivered'),
-(0, 1, '2019-11-24', 18, 'Cash On Delivery', 'Undelivered'),
-(0, 1, '2019-11-24', 18, 'Cash On Delivery', 'Undelivered'),
-(0, 1, '2019-11-24', 18, 'Cash On Delivery', 'Undelivered'),
-(0, 1, '2019-11-24', 22, 'Cash On Delivery', 'Undelivered'),
-(1, 3, '2019-11-24', 22, 'Cash On Delivery', 'Undelivered'),
-(0, 1, '2019-11-24', 18, 'Cash On Delivery', 'Undelivered');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `11cart`
---
-
-CREATE TABLE `11cart` (
-  `productID` int(10) DEFAULT NULL,
-  `amount` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `11purchases`
---
-
-CREATE TABLE `11purchases` (
-  `productID` int(10) DEFAULT NULL,
-  `amount` int(10) DEFAULT NULL,
-  `datePurchase` date DEFAULT NULL,
-  `salesID` int(10) DEFAULT NULL,
-  `paymentMethod` varchar(16) DEFAULT NULL,
-  `orderStatus` varchar(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
 
 -- --------------------------------------------------------
 
@@ -140,11 +50,16 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`productID`, `productTitle`, `productAuthor`, `productPublisher`, `productType`, `productLanguage`, `productDateReceived`, `productDatePublished`, `productGenre`, `productImage`, `productStock`, `productPrice`, `productDescription`) VALUES
 (0, 'Delivery Charge', 'admin', 'null', 'null', 'null', '0000-00-00', '0000-00-00', 'null', 'null', 0, 50.00, 'null'),
-(1, 'Kono Subarashii Sekai ni Shukufuku wo! Volume 10', 'Natsume Akatsuki', 'Yen Press', 'Light Novel', 'Japanese', '2019-11-07', '2016-11-01', 'fantasy, action', 'product_image/v10cover.jpg', 45, 300.00, 'After Belzerg\'s financial support is cut off, Iris is forced into an arranged marriage with Elroad\'s crown prince in an attempt to regain the funding Belzerg so desperately needs to continue repelling the Demon King\'s invasion. With even this maneuver not guaranteed to succeed, Iris calls for help from the one person which common sense never seems to apply to.'),
-(2, 'Dungeon ni Deai o Motomeru no wa Machigatteiru Darou ka Volume 8', 'Fujino Omori', 'SB Creative', 'Light Novel', 'Japanese', '2019-11-14', '2015-06-15', 'on, on, on, on', 'product_image/DanMachi_Light_Novel_Volume_8_Cover.png', 18, 300.00, 'The royal army led by the War God Ares. The number of the army heading to Orario numbers 30,000. Against the sound of the incoming army, Orario - didn\'t change.\r\nWhile the invaders outside let out cries of despair against the too strong adventurers, Orario spent peaceful days.\r\nThe Pallum\'s marriage proposal, the lovely bodyguard, the town girl\'s secret, the love songs to the Gods - and the love song that a Goddess spins.\r\nA daily life part that Gods and children give!'),
-(3, 'No Game No Life Volume 1', 'Yuu Kamiya', 'Yen Press', 'Light Novel', 'English', '2019-11-23', '2019-11-22', 'on, on, on, on, on', 'product_image/ngnl-vol1.png', -3, 150.00, 'Sora and Shiro are two hikikomori step-siblings who are known in the online gaming world as Blank, an undefeated group of gamers. One day, they are challenged to a game of chess by Tet, a god from another reality. The two are victorious and are offered to live in a world that centers around games. They accept, believing it to be a joke, and are summoned to a reality known as Disboard.'),
-(4, 'Yahari Ore no Seishun Love Come wa Machigatteiru', 'Watari Wataru', 'Yen Press', 'Light Novel', 'English', '2019-11-23', '2019-11-22', 'on, on', 'product_image/oregairu-vol1--eng.png', 3, 120.00, 'The story follows: the pragmatic Hachiman Hikigaya, Yukino Yukinoshita and friendly Yui Yuigahama, who, despite their varying personalities and ideals, offer help and advice to others as part of their school\'s Service Club. It largely depicts various social situations faced by teens in a high school setting and the psychology driving their interactions.'),
-(5, 'Yahari Ore no Seishun Love Come wa Machigatteiru', 'Watari Wataru', 'Shogakukan', 'Light Novel', 'Japanese', '2019-11-22', '2019-11-21', 'on, on', 'product_image/oregairu-vol1-jap.png', 0, 100.00, 'The story follows: the pragmatic Hachiman Hikigaya, Yukino Yukinoshita and friendly Yui Yuigahama, who, despite their varying personalities and ideals, offer help and advice to others as part of their school\'s Service Club. It largely depicts various social situations faced by teens in a high school setting and the psychology driving their interactions.');
+(6, 'DragonBall Vol.1', 'Akira Toriyama', 'Viz Media', 'Manga', 'English', '2019-12-01', '2019-11-30', 'adventure, action, fantasy', 'product_image/DBallv1.png', 5, 150.00, 'Before there was Dragon Ball Z, there was Akira Toriyama\'s action epic Dragon Ball, starring the younger version of Son Goku and all the other Dragon Ball Z heroes! Meet a naive young monkey-tailed boy named Goku, whose quiet life changes when he meets Bulma, a girl who is on a quest to collect seven \"Dragon Balls.\" If she gathers them all, an incredibly powerful dragon will appear and grant her one wish. But the precious orbs are scattered all over the world, and Bulma needs Goku\'s help (and his super-strength)! With a magic staff for a weapon and a flying cloud for a ride, Goku sets out on the adventure of a lifetime...'),
+(7, 'Bleach Vol.25', 'Tite Kubo', 'Shounen Jump', 'Manga', 'Japanese', '2019-12-01', '2019-11-30', 'adventure, action, fantasy', 'product_image/bleachvol25.jpg', 10, 100.00, 'Ichigo\'s recent battles with the Arrancars have proven that if he wants to protect his friends he must get stronger, and the only way to do that is to control his inner Hollow. Ichigo turns to the Visoreds, ex-Soul Reapers who have been Hollowfied, to teach him. But before his training begins, Ichigo must do battle against his Hollow self--winner takes his soul!'),
+(8, 'Saint Young Men Vol.1', 'Hikaru Nakamura', 'Kodansha', 'Manga', 'English', '2019-12-01', '2019-11-30', 'slice of life', 'product_image/saintyoungmen.jpg', 26, 150.00, 'Saint Young Men is a Japanese slice of life comedy manga series written and illustrated by Hikaru Nakamura. Its plot involves Jesus Christ and Gautama Buddha, who are living as roommates in an apartment in Tokyo.'),
+(9, 'The Way of House Husband Vol.1', 'Kouske Oono', 'Viz Media', 'Manga', 'English', '2019-12-01', '2019-11-30', 'slice of life', 'product_image/WayHouseHusband.jpg', 20, 150.00, 'He was the fiercest member of the yakuza, a man who left countless underworld legends in his wake. They called him â€œthe Immortal Dragon.â€ But one day he walked away from it all to travel another pathâ€”the path of the househusband! The curtain rises on this cozy yakuza comedy!'),
+(10, 'Naruto Vol.1', 'Masashi Kishimoto', 'Shounen Jump', 'Manga', 'Japanese', '2019-12-01', '2019-11-30', 'adventure, action, fantasy', 'product_image/naruto-vol1.png', 10, 150.00, '\"The legend. Twelve years ago, the peaceful village of Konohagakure was terroized by a nine-tailed fox demon. The town\'s ninja fought bravely against the ferocious spirit. After a long battle, the fox demon was captured by the Hokage, the village leader and most powerful ninja...'),
+(11, 'No Game No Life', 'Yuu Kamiya', 'Yen Press', 'Light Novel', 'English', '2019-12-01', '2019-11-30', 'adventure, fantasy', 'product_image/ngnl-vol1.png', 5, 120.00, 'In this fantasy world, everything\'s a game--and these gamer siblings play to win!\r\n\r\nMeet Sora and Shiro, a brother and sister who are loser shut-ins by normal standards. But these siblings don\'t play by the rules of the \"crappy game\" that is average society. In the world of gaming, this genius pair reigns supreme, their invincible avatar so famous that it\'s the stuff of urban legend. So when a young boy calling himself God summons the siblings to a fantastic alternate world where war is forbidden and all conflicts--even those involving national borders--are decided by the outcome of games, Sora and Shiro have pretty much hit the jackpot. But they soon learn that in this world, humanity, cornered and outnumbered by other species, survives within the confines of one city. Will Sora and Shiro, two failures at life, turn out to be the saviors of mankind? Let the games begin...!'),
+(12, 'Shield Hero Vol.8', 'Aneko Yusagi', 'Kadokawa', 'Light Novel', 'Japanese', '2019-12-01', '2019-11-30', 'adventure, action, fantasy', 'product_image/shieldherovol8.jpg', 5, 110.00, 'At a critical moment, Naofumi and the others are saved by a young girl. Ostâ€™s sacrifice is enough to save them from their battle to the death with Kyo, but Kyo\'s able to escape to another world. Naofumi attempts to follow him, but he awakens to find himself in prison! His friends are scattered and he finds himself back at level one. How will Naofumi survive the new challenges that await him?'),
+(13, 'Kono Subarashii Sekai Ni Shuufuku Wo!', 'Natsume Akatsuki', 'Kadokawa', 'Light Novel', 'Japanese', '2019-12-01', '2019-11-30', 'adventure, action, fantasy', 'product_image/v10cover.jpg', 15, 120.00, 'After Belzerg\'s financial support is cut off, Iris is forced into an arranged marriage with Elroad\'s crown prince in an attempt to regain the funding Belzerg so desperately needs to continue repelling the Demon King\'s invasion. With even this maneuver not guaranteed to succeed, Iris calls for help from the one person which common sense never seems to apply to.'),
+(14, 'Is It Wrong to Try to Pick Up Girls in a Dungeon?', 'Fujino Omori', 'Yen Press', 'Light Novel', 'English', '2019-12-01', '2019-11-30', 'adventure, romance, action, fantasy', 'product_image/DanMachi_Light_Novel_Volume_8_Cover.png', 14, 160.00, '30 kilometers east of Orario, Ares and his men marched to the city to start another war, but despite this the citizens of the city weren\'t concerned at all as there was nothing to worry about. As Orario had a dungeon, their adventurers were far stronger, and therefore they were used to easily clear out the enemy. Taking advantage of this, merchants, including Gods like Dian Cecht, swarmed the enemy camp, eager to sell them what they could manage to forcefully sell them. This included prostitutes like Aisha and Samira who scoured the battlefield for good men that they could enjoy. '),
+(15, 'My Youth Romantic Comedy Is Wrong, As I Expected', 'Wataru Watari', 'Yen Press', 'Light Novel', 'English', '2019-12-01', '2019-11-30', 'romance, slice of life', 'product_image/oregairu-vol1--eng.png', 5, 180.00, 'Hachiman Hikigaya is an apathetic high school student with narcissistic and semi-nihilistic tendencies. He firmly believes that joyful youth is nothing but a farce, and everyone who says otherwise is just lying to themselves.\r\n\r\nIn a novel punishment for writing an essay mocking modern social relationships, Hachiman\'s teacher forces him to join the Volunteer Service Club, a club that aims to extend a helping hand to any student who seeks their support in achieving their goals. With the only other club member being the beautiful ice queen Yukino Yukinoshita, Hachiman finds himself on the front line of other people\'s problemsâ€”a place he never dreamed he would be. As Hachiman and Yukino use their wits to solve many students\' problems, will Hachiman\'s rotten view of society prove to be a hindrance or a tool he can use to his advantage?');
 
 -- --------------------------------------------------------
 
@@ -160,27 +75,6 @@ CREATE TABLE `sales` (
   `paymentMethod` varchar(16) NOT NULL,
   `deliveryStatus` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
---
--- Dumping data for table `sales`
---
-
-INSERT INTO `sales` (`salesID`, `amount`, `salesDate`, `invoice`, `paymentMethod`, `deliveryStatus`) VALUES
-(9, 500, '2019-11-22', 10, '', ''),
-(10, 170, '2019-11-22', 10, '', ''),
-(11, 350, '2019-11-22', 10, '', ''),
-(12, 200, '2019-11-23', 10, 'Cash On Delivery', 'Delivered'),
-(13, 290, '2019-11-23', 10, 'Cash On Delivery', 'Delivered'),
-(14, 0, '2019-11-23', 10, 'Cash On Delivery', 'Undelivered'),
-(15, -550, '2019-11-24', 10, '', 'Undelivered'),
-(16, 0, '2019-11-24', 10, 'Cash On Delivery', 'Cancelled'),
-(17, 770, '2019-11-24', 10, 'Cash On Delivery', 'Undelivered'),
-(18, 650, '2019-11-24', 10, 'Cash On Delivery', 'Undelivered'),
-(19, 650, '2019-11-24', 10, 'Cash On Delivery', 'Undelivered'),
-(20, 650, '2019-11-24', 10, 'Cash On Delivery', 'Undelivered'),
-(21, 650, '2019-11-24', 10, 'Cash On Delivery', 'Undelivered'),
-(22, 1550, '2019-11-24', 10, 'Cash On Delivery', 'Undelivered'),
-(23, 650, '2019-11-24', 10, 'Cash On Delivery', 'Undelivered');
 
 -- --------------------------------------------------------
 
@@ -201,10 +95,7 @@ CREATE TABLE `useraccounts` (
 --
 
 INSERT INTO `useraccounts` (`userID`, `userEmail`, `userPassword`, `userType`, `status`) VALUES
-(1, 'Admin', 'Admin', 'Admin', ''),
-(2, 'carloangeles342@gmail.com', 'asdfjklh', 'user', 'Inactive'),
-(10, 'ethanreiarevalo@gmail.com', 'hanirokyu', 'user', 'Active'),
-(11, 'toniaguilar14@gmail.com', 'kahitanona', 'user', 'Active');
+(1, 'Admin', 'Admin', 'Admin', '');
 
 -- --------------------------------------------------------
 
@@ -222,15 +113,6 @@ CREATE TABLE `userdetails` (
   `sex` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
 
---
--- Dumping data for table `userdetails`
---
-
-INSERT INTO `userdetails` (`email`, `fName`, `lName`, `address`, `contactNo`, `birthdate`, `sex`) VALUES
-('carloangeles342@gmail.com', 'Carlo', 'Angeles', 'Alfonso, Cavite', '2147483647', '1996-11-01', 'Male'),
-('ethanreiarevalo@gmail.com', 'Ethan Rei', 'Arevalo', 'Regina Ville 2000', '2147483647', '1996-08-09', 'Female'),
-('toniaguilar14@gmail.com', 'Jan Anthony', 'Aguilar', 'Paliparan, DasmariÃ±as City', '09776844592', '1998-01-15', 'Male');
-
 -- --------------------------------------------------------
 
 --
@@ -243,13 +125,6 @@ CREATE TABLE `vouchers` (
   `voucherAmount` int(5) NOT NULL,
   `voucherDiscount` float(6,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
---
--- Dumping data for table `vouchers`
---
-
-INSERT INTO `vouchers` (`voucherID`, `voucherName`, `voucherAmount`, `voucherDiscount`) VALUES
-(3922, 'sample voucher', 10, 0.00);
 
 --
 -- Indexes for dumped tables
@@ -281,19 +156,19 @@ ALTER TABLE `useraccounts`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `salesID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `salesID` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `useraccounts`
 --
 ALTER TABLE `useraccounts`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
